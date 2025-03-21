d3b0f717a5b1b25a107619a828f524100a05e2b4
Scylla186 <delriojey186@gmail.com>
2025-03-21 11:08:21 -0500

    Add files via upload

---
+++ asciixddddd/src/main/java/com/mycompany/asciixddddd/Asciixddddd.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.asciixddddd;
+
+import java.io.*;
+
+import java.util.*;
+import java.lang.*;
+import java.util.Scanner;
+/**
+ *
+ * @author Scylla
+ */
+public class Asciixddddd {
+ static void ASCIISentence(String str)
+    {
+        int l = str.length();
+        int convert;
+        for (int i = 0; i < l; i++) {
+            convert = str.charAt(i);
+            System.out.print((convert+1)+" ");
+        }
+    }
+    public static void main(String[] args) throws IOException {
+
+        
+    
+         Convertidorasciixddd FRAMES = new Convertidorasciixddd();
+     FRAMES.pack();
+     FRAMES.setLocationRelativeTo(null);
+     FRAMES.setResizable(false);
+     FRAMES.setVisible(true);
+     
+     
+    
+}
+}
+
